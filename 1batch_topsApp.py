2ef43c2d9d9092af09353721b88bde69dc880fb1
fanchengyan <fanchy14@lzu.edu.cn>
2022-09-25 13:49:25 +0800

    normal update

---
+++ 1batch_topsApp.py
@@ -0,0 +1,185 @@
+# %%
+import csv
+import logging
+import logging.config
+import os
+from datetime import datetime
+from pathlib import Path
+from tqdm import tqdm
+from isceobj.XmlUtil import FastXML as xml
+
+logging.config.fileConfig(os.path.join(os.getcwd(), 'logging.conf'))
+logger = logging.getLogger('batch_tops')
+
+#########################################################
+#  Modify following variables according to your condition
+# ========================================================
+# Folder contains insar data
+sar_dir = '/data/sentinel1/data'
+# Folder contains DEM data
+dem = '/data/sentinel1/DEM/demLat_N38_N40_Lon_E097_E101.dem'
+# dem = None
+# Folder contains Precise orbits
+orbit = '/data/sentinel1/aux/aux_poeorb'
+# Folder contains Auxiliary data
+aux = '/data/sentinel1/aux/aux_cal'
+
+range_looks = 20
+azimuth_looks = 4
+filter_strength = 0.01
+swath_number = None
+unwrapper_name = 'snaphu'
+
+roi = [38.4573, 39.0911, 98.6675, 99.3582]
+# [S, N, W, E] convention (lat/lon)
+
+def generate_topsApp_xml(
+        reference_dir, secondary_dir, reference_file, secondary_file, roi, xml_file):
+    '''导出干涉对xml配置文件'''
+
+    # reference and secondary information
+    fix_info = {"orbit directory": orbit,
+                "auxiliary data directory": aux}
+    if swath_number is not None:
+        fix_info.update({"swath number": fix_info})
+
+    reference = {"safe": reference_file,
+                 "output directory": reference_dir}
+    reference.update(fix_info)
+
+    secondary = {"safe": secondary_file,
+                 "output directory": secondary_dir}
+    secondary.update(fix_info)
+
+    tops = xml.Component('topsinsar')
+    # parameters
+    tops["Sensor name"] = 'SENTINEL1'
+
+    tops['reference'] = reference
+    tops['secondary'] = secondary
+
+    if roi:
+        tops["region of interest"] = roi
+    if dem:
+        tops["demFilename"] = Path(dem).name
+    tops["use GPU"] = True
+    tops["range looks"] = range_looks
+    tops["azimuth looks"] = azimuth_looks
+    tops["do unwrap"] = True
+    tops["unwrapper name"] = unwrapper_name
+    tops["filter strength"] = filter_strength
+
+    tops.writeXML(xml_file, root='topsApp')
+
+
+def mapper_date_data(folder, suffix):
+    '''映射日期到数据文件
+    Select the data files from the folder,and then return 
+    a mapper from year to data file
+    '''
+    files_s1 = sorted([i for i in os.listdir(folder)
+                      if i.split('.')[-1] == suffix])
+    dates = [i[17:25] for i in files_s1]
+    mapper = dict()
+    for i, date in enumerate(dates):
+        mapper.update({date: files_s1[i]})
+    return mapper
+
+
+def load_interferogram_pairs(path_list, delimiter=','):
+    '''加载干涉对文件'''
+    with open(path_list) as f:
+        ms_list = list(csv.reader(f, delimiter=delimiter))
+    return ms_list
+
+
+def day_interval(date_start, date_end):
+    date_start = datetime.strptime(date_start, '%Y%m%d')
+    date_end = datetime.strptime(date_end, '%Y%m%d')
+    interval = (date_end - date_start).days
+
+    return interval
+
+
+def generate_interferogram_pairs(sar_dir,  pair_file, sar_suffix='zip',
+                                 max_interval=2, max_day=180, delimiter=','):
+    '''导出干涉对文件。
+    Generate and save a reference-secondary pair automatically. Time interval 
+    of reference-secondary pair is 1 and 2
+
+    dates: str 
+        date string with format of '%Y%m%d'
+    max_interval: int
+        干涉对最大相邻获取间隔, 相邻间隔为1
+    max_day:int
+        干涉对最大的间隔天数
+    pair_file: str
+        the path of file out.if None, will not save. 
+    delimiter: str
+        reference与secondary日期之间的分割符号
+    '''
+    mapper = mapper_date_data(sar_dir, suffix=sar_suffix)
+    dates = sorted(mapper.keys())
+
+    num = len(dates)
+    reference_secondary_pair_list = []
+    for i, date in enumerate(dates):
+        dti_temp = 1
+        while dti_temp <= max_interval:
+            if i + dti_temp < num:
+                if day_interval(date, dates[i + dti_temp]) < max_day:
+                    reference_secondary_pair_list.append(
+                        [date, dates[i + dti_temp]])
+                    dti_temp += 1
+                else:
+                    break
+            else:
+                break
+
+    with open(pair_file, 'w', newline='') as f:
+        csv_writer = csv.writer(f, delimiter=delimiter)
+        csv_writer.writerows(reference_secondary_pair_list)
+
+
+# %%         generate_interferogram_pairs
+
+home_dir = Path('/data/sentinel1/result')
+pair_file = home_dir / 'ifg_pairs.csv'
+
+generate_interferogram_pairs(sar_dir, pair_file, sar_suffix='zip',
+                             max_interval=3, max_day=180, delimiter=',')
+
+# %%   batch run topsApp.py
+ifg_pairs = load_interferogram_pairs(pair_file)
+mapper = mapper_date_data(sar_dir, suffix='zip')
+
+for i, ifg in enumerate(tqdm(ifg_pairs)):
+    reference, secondary = ifg
+    reference_file = os.path.join(sar_dir, mapper[reference])
+    secondary_file = os.path.join(sar_dir, mapper[secondary])
+
+    ifg_name = f'{reference}_{secondary}'
+    ifg_dir = home_dir / f'interferogams/{ifg_name}'
+    if not ifg_dir.is_dir():
+        ifg_dir.mkdir(parents=True)
+
+    # create xml file
+    reference_dir = ifg_dir / f'{reference}'
+    secondary_dir = ifg_dir / f'{secondary}'
+    xml_file = ifg_dir / 'topsApp.xml'
+
+    generate_topsApp_xml(reference_dir, secondary_dir,
+                         reference_file, secondary_file, roi, xml_file)
+
+    # cd ifg folder
+    os.chdir(ifg_dir)
+
+    # link dem file to current folder
+    print(ifg_name)
+    cmd_link = f'ln -sf {str(Path(dem).parent)}/* ./'
+    os.system(cmd_link)
+
+    # execute topsApp.py
+    cmd_tops = f'topsApp.py {xml_file}'
+    logger.info(cmd_tops)
+    os.system(cmd_tops)
---
+++ 2tops2mintpy.py
@@ -0,0 +1,158 @@
+# %%
+import os
+from pathlib import Path
+from tqdm import tqdm
+
+ifg_home = Path('/data/sentinel1/result/interferogams')
+stack_home = Path('/data/sentinel1/result/stacks')
+
+
+# %%
+############  make dirs #############
+def ensure_folder(folder):
+    if not folder.is_dir():
+        folder.mkdir(parents=True)
+
+
+baseline_dir = stack_home / 'baselines'
+reference_dir = stack_home / 'reference'
+secondarys_dir = stack_home / 'secondarys'
+mintpy_dir = stack_home / 'mintpy'
+
+merged_dir = stack_home / 'merged'
+geom_reference_dir = merged_dir / 'geom_reference'
+interferograms_dir = merged_dir / 'interferograms'
+
+stack_fld_list = [baseline_dir, reference_dir,
+                  secondarys_dir, mintpy_dir,
+                  merged_dir, geom_reference_dir,
+                  interferograms_dir]
+
+for fld in stack_fld_list:
+    ensure_folder(fld)
+
+
+# %%       link files
+def looks(vrt_file, rdr_file, nlook_a, nlook_r):
+    os.system(f'looks.py -i {vrt_file} -o {rdr_file} -a {nlook_a} -r {nlook_r}')
+
+ifgs_files = sorted(ifg_home.iterdir())
+for file in tqdm(ifgs_files, desc='link files'):
+    name = file.name
+    ref_name, sec_name = name.split('_')
+    merged = file / 'merged'
+    i = 0
+    if merged.exists():
+        os.chdir(file)
+        if i == 0:
+            ####################################################################
+            # For the folder 'reference' and 'merged/geom_reference', you only 
+            # need to get it from the first interferogram            
+            #####################################################################
+            i += 1
+            
+            ######### link to 'reference' folder ######
+            ref_dir = file / ref_name
+            ref_cmd = f'ln -sf {ref_dir}/* {reference_dir}/'
+            os.system(ref_cmd)
+
+            ####################################################################
+            #     multi-looking for geo_info files to 'geom_reference' folder
+            #####################################################################
+            # For the files that contain the geo info of ifgs, only 'los' was 
+            # processed well (multi-looked and with '.rdr' format). We need 
+            # to convert the full resolution geo info files into multi-looked 
+            # files with ISCE format('.rdr'). In this script, azimuth/range looks
+            # are: 4*20.
+			#####################################################################
+            looks(merged.joinpath('z.rdr.full.xml'),
+                    geom_reference_dir.joinpath('hgt.rdr'),
+                    4,20)
+            looks(merged.joinpath('lat.rdr.full.xml'),
+                    geom_reference_dir.joinpath('lat.rdr'),
+                    4,20)
+            looks(merged.joinpath('lon.rdr.full.xml'),
+                    geom_reference_dir.joinpath('lon.rdr'),
+                    4,20)
+            looks(merged.joinpath('los.rdr.full.xml'),
+                    geom_reference_dir.joinpath('los.rdr'),
+                    4,20)
+
+
+        # ########## link ifgs  ################
+        ifg_out = interferograms_dir / name
+        ensure_folder(ifg_out)
+
+        # ifgs
+        ln_cmd1 = f'ln -sf {merged}/filt_* {ifg_out}/'
+        os.system(ln_cmd1)
+
+        # cor
+        ln_cmd2 = f'ln -sf {merged}/phsig.cor* {ifg_out}/'
+        os.system(ln_cmd2)
+        # ########## link secondarys  ################
+        sec_dir = file / sec_name
+        sec_cmd = f'ln -sf {sec_dir} {secondarys_dir}/'
+        os.system(sec_cmd)
+
+#%%
+########## generate_baseline  #############
+def get_dates_from_ifgs(ifgs):
+    dates_all = []
+    for ifg in ifgs:
+        dates_all.extend(ifg.split('_'))
+    dates = sorted(set(dates_all))
+    return dates
+
+
+ifgs = [i.name for i in ifgs_files]
+dates = get_dates_from_ifgs(ifgs)
+
+ref_name = dates[0]
+ref_dir = ifgs_files[0] / ref_name
+for sec_name in tqdm(dates[1:], desc='generate baselines'):
+    sec_dir = secondarys_dir / sec_name
+
+    bl_name = f'{ref_name}_{sec_name}'
+    bl_dir = baseline_dir / bl_name
+    ensure_folder(bl_dir)
+
+    bl_file = bl_dir / f'{bl_name}.txt'
+    bl_cmd = f'/data/isce2/contrib/stack/topsStack/computeBaseline.py -m {ref_dir}/ -s {sec_dir}/ -b {bl_file}'
+    os.system(bl_cmd)
+
+
+# %%        prep_isce.py
+
+os.chdir(reference_dir)
+prep_cmd = ('/root/miniconda3/bin/prep_isce.py'
+            f' -f "{interferograms_dir}/*/filt_*.unw"'
+            f' -m {reference_dir}/IW2.xml'
+            f' -b {baseline_dir}/'
+            f' -g {geom_reference_dir}/')
+print(prep_cmd)
+os.system(prep_cmd)
+
+# %%       write options file
+
+opt_file = mintpy_dir / 'mintpy_data_options.txt'
+
+opt_info = f'''mintpy.load.processor        = isce
+##---------for ISCE only:
+mintpy.load.metaFile         = {reference_dir}/IW*.xml
+mintpy.load.baselineDir      = {baseline_dir}
+##---------interferogram datasets:
+mintpy.load.unwFile          = {interferograms_dir}/*/filt_*.unw
+mintpy.load.corFile          = {interferograms_dir}/*/phsig.cor
+mintpy.load.connCompFile     = {interferograms_dir}/*/filt_*.unw.conncomp
+##---------geometry datasets:
+mintpy.load.demFile          = {geom_reference_dir}/hgt.rdr
+mintpy.load.lookupYFile      = {geom_reference_dir}/lat.rdr
+mintpy.load.lookupXFile      = {geom_reference_dir}/lon.rdr
+mintpy.load.incAngleFile     = {geom_reference_dir}/los.rdr
+mintpy.load.azAngleFile      = {geom_reference_dir}/los.rdr
+# mintpy.load.shadowMaskFile   = {geom_reference_dir}/shadowMask.rdr
+'''
+
+with open(opt_file, 'w') as f:
+    f.write(opt_info)
